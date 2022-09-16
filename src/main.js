58b2bf468ffb0617a689de6dfd2305aec31fbd50
duc-rikai <duc.nguyen@rikai.technology>
2022-09-16 22:58:56 +0700

    Get Data API

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
-import App from "./App.vue";
-import router from "./router";
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/main.css";
 
 Vue.use(ElementUI);
